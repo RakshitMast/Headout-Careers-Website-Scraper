d99dd71fa3dbe474c4448c055589f5966588b634
Rakshit <rakshitmast@gmail.com>
2024-07-03 18:08:11 +0530

    Update main.py
    added path to chrome driver

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 
 # Function to fetch webpage content using Selenium
 def fetch_webpage(url):
+
+    
+    
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     
-    driver = webdriver.Chrome(options=options)
+    chrome_driver_path = os.path.join(os.getcwd(), 'chromedriver.exe')
+    service = Service(executable_path=chrome_driver_path)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(url)
     time.sleep(1)
 
